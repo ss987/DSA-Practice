6e833265252b814f73effcd02231b160110a607c
ss987 <98395295+ss987@users.noreply.github.com>
2023-10-15 22:13:19 +0530

    Update buyAndSellStock.cpp

--- Two Pointer/buyAndSellStock.cpp
+++ Two Pointer/buyAndSellStock.cpp
@@ -1,4 +1,22 @@
-    int maxProfit(vector<int>& prices) {
+//Level 1
+int maxProfit(vector<int>& prices) {
+        if(prices.size()<2)
+        {
+            return 0;
+        }
+
+        int minprice=prices[0];
+
+        int pro=0;
+        for (int i=0;i<prices.size();i++){
+            pro=max(pro,prices[i]-minprice);
+            minprice=min(minprice,prices[i]);
+        }
+        return(pro);
+    }
+//Level 2
+
+int maxProfit(vector<int>& prices) {
         int i=0,sum=0,j=1;
         while(j<prices.size()){
             if(prices[i]<prices[j]){
