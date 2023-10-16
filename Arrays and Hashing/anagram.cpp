dbd0f1c96f2ebdca839c40c71f4ae82a8dfd8b67
ss987 <98395295+ss987@users.noreply.github.com>
2023-10-17 00:36:48 +0530

    Create anagram.cpp

---
+++ Arrays and Hashing/anagram.cpp
@@ -0,0 +1,15 @@
+bool isAnagram(string s, string t) {
+      unordered_map<char,int> freq;
+      for(auto i:s){
+          freq[i]++;
+      } 
+      for(auto i:t){
+          freq[i]--;
+      } 
+      for(auto i:freq){
+          if(i.second!=0){
+              return false;
+          }
+      } 
+      return true;
+    }
