c9e967b11e4104ece484158c88c6e98b84657103
ss987 <98395295+ss987@users.noreply.github.com>
2024-01-27 10:20:27 +0530

    Create deleteLast.cpp

---
+++ Linked list/deleteLast.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class Node {
+ * public:
+ *     int data;
+ *     Node *next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node *next) : data(x), next(next) {}
+ * };
+ */
+
+Node *deleteLast(Node *list){
+    // Write your code here
+    Node* ptr=list;
+    while(ptr->next->next!=nullptr){
+        ptr=ptr->next;
+    }
+    ptr->next=nullptr;
+    return list;
+}
