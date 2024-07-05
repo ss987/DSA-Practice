6645a35e59559758b2c8d481b0904f54479af578
ss987 <98395295+ss987@users.noreply.github.com>
2024-07-06 01:05:29 +0530

    Create climbStairs.cpp

---
+++ Dynamic Prg/climbStairs.cpp
@@ -0,0 +1,10 @@
+int climbStairs(int n) {
+        int prev1=1,prev2=1,cur;
+        if (n<=1) return n;
+        for(int i=2;i<=n;i++){
+            cur=prev1+prev2;
+            prev1=prev2;
+            prev2=cur;
+        }
+        return cur;
+    }
