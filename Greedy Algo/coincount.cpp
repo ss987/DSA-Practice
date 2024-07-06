94ee46baed2dfa3fc413cd2933a6ed7494e345de
ss987 <98395295+ss987@users.noreply.github.com>
2024-07-07 00:11:14 +0530

    Update coincount.cpp

--- Greedy Algo/coincount.cpp
+++ Greedy Algo/coincount.cpp
@@ -1,1 +1,16 @@
-cpp
+int minCoins(vector<int> &coins, int M, int V) 
+	{ 
+	    // Your code goes here
+    //works only if sum of 2 denomination is less than other. Fails otherwise
+	    int c=0;
+	    for(int i=0;i<M;i++){
+	        while(V>=coins[i]){
+	            V-=coins[i];
+	            c++;
+	        }
+	    }
+	    if (V > 0) {
+            return -1;
+        }
+	    return c;
+	} 
