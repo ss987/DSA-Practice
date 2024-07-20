3ba59000021d55dc45e0ef264792e044e9749be0
ss987 <98395295+ss987@users.noreply.github.com>
2024-07-20 22:50:21 +0530

    Create findKthLargest.cpp

---
+++ Priority Queue/findKthLargest.cpp
@@ -0,0 +1,10 @@
+int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq(nums.begin(),nums.begin()+k);
+        for(int i=k;i<nums.size();i++){
+            if(nums[i]>pq.top()){
+                pq.pop();
+                pq.push(nums[i]);                
+            }
+        }
+        return pq.top();
+    }
